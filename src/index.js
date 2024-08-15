97183974ae43aab79fe9ec136ba42a2774287a7b
Sergii Polushkin <seregapola@gmail.com>
2024-08-15 13:52:10 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let reversedString = parseInt(
+        n.toString().split("").reverse().join(""),
+        10
+    );
+    return reversedString;
+};
